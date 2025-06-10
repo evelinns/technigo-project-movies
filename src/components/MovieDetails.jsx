1ee09f3967d47478d4e10ebd3dae6840fc57230a
evelinns <evelinn.idenfors@gmail.com>
2025-06-10 08:59:47 +0100

    loading data on movide details page

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,3 +1,30 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 export const MovieDetails = () => {
-  return <div>Movie Details</div>
+  const API_KEY = import.meta.env.VITE_MOVIE_API_KEY;
+  const params = useParams();
+  const [movieDetails, setMovieDetails] = useState([])
+
+  const movieDetailsApi = `https://api.themoviedb.org/3/movie/${params.movieId}?api_key=${API_KEY}&language=en-US`
+
+  const fetchMoveDetails = () => {
+    fetch(movieDetailsApi)
+    .then((res) => {
+      return res.json()
+    }).then(data => setMovieDetails(data))
+  }
+
+  useEffect(() => {
+    fetchMoveDetails()
+  }, [])
+
+  console.log(movieDetails)
+
+  return (
+    <>
+      <h1>Movie Details</h1>
+      {movieDetails.original_title}
+    </>
+  )
 }
